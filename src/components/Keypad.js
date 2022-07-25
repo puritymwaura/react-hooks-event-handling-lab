901b344f9ecc0c5c3de57ab12bd2cac126615f05
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-07-25 08:33:24 +0300

    commit tree 6a7eef1d865dc9eb10469b6e2ecfe8f5b9573367

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
 // Code Keypad Component Here
+import React from 'react'
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+
+  function handlePassword(){
+    console.log('Entering password...')
+
+  }
+
+  return (
+    <input type="password" onChange={handlePassword} ></input>
+  )
 }
 
-export default Keypad;
+export default Keypad
